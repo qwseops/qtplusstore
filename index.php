6dc1a6e62c8b767e24ff652a55cf76a7822538a4
qwseops <40327168+qwseops@users.noreply.github.com>
2023-03-27 00:48:03 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE .php>
+<!DOCTYPE php>
 <php style="font-size: 16px;" lang="en"><head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
@@ -362,4 +362,4 @@ Impaired Rats. Journal of Audiology and Speech Pathology. 1998.&nbsp; &nbsp; &nb
       </a>. 
     </section>
   
-</body></.php>
+</body></php>
